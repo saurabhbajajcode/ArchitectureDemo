11a9c47ecea0978bf8eb7ab0ba432b1e0a2bae41
Saurabh Bajaj <saurabhbsd@gmail.com>
2021-02-14 01:48:07 +0530

    remove scene delegate. add api handler, app manager and touchblocker files

---
+++ ArchitectureDemo/Miscellaneous/AppManager.swift
@@ -0,0 +1,56 @@
+//
+//  AppManager.swift
+//  ArchitectureDemo
+//
+//  Created by Saurabh Bajaj on 13/02/21.
+//  Copyright © 2021 ArchitectureDemo. All rights reserved.
+//
+
+import UIKit
+
+class AppManager: NSObject {
+
+    private static var _sharedInstance = AppManager()
+    static var shared: AppManager {
+        return AppManager._sharedInstance
+    }
+
+    private var touchBlocker: TouchBlocker?
+
+    private override init() {
+        super.init()
+    }
+
+    /// Reinstantiates AppManager object.
+    func resetAppManager() {
+        AppManager._sharedInstance = AppManager()
+    }
+
+    fileprivate func setupTouchBlocker() {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        touchBlocker = TouchBlocker(frame: CGRect.zero)
+        touchBlocker?.translatesAutoresizingMaskIntoConstraints = false
+        appDelegate?.window?.addSubview(touchBlocker!)
+        appDelegate?.window?.addConstraint(NSLayoutConstraint(item: touchBlocker!, attribute: .top, relatedBy: .equal, toItem: appDelegate?.window, attribute: .top, multiplier: 1, constant: 0))
+        appDelegate?.window?.addConstraint(NSLayoutConstraint(item: touchBlocker!, attribute: .left, relatedBy: .equal, toItem: appDelegate?.window, attribute: .left, multiplier: 1, constant: 0))
+        appDelegate?.window?.addConstraint(NSLayoutConstraint(item: touchBlocker!, attribute: .right, relatedBy: .equal, toItem: appDelegate?.window, attribute: .right, multiplier: 1, constant: 0))
+        appDelegate?.window?.addConstraint(NSLayoutConstraint(item: touchBlocker!, attribute: .bottom, relatedBy: .equal, toItem: appDelegate?.window, attribute: .bottom, multiplier: 1, constant: 0))
+    }
+
+    func showLoading() {
+        DispatchQueue.main.async { [self] in
+            guard touchBlocker != nil else {
+                setupTouchBlocker()
+                showLoading()
+                return
+            }
+            let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            appDelegate?.window?.bringSubviewToFront(self.touchBlocker!)
+            touchBlocker!.showLoading()
+        }
+    }
+
+    func hideLoading() {
+        touchBlocker?.hideLoading()
+    }
+}
---
+++ ArchitectureDemo/Miscellaneous/InternetConnection.swift
@@ -0,0 +1,68 @@
+//
+//  InternetConnection.swift
+//  ArchitectureDemo
+//
+//  Created by Saurabh Bajaj on 13/02/21.
+//  Copyright © 2021 ArchitectureDemo. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+public enum IJReachabilityType {
+    case wwan,
+    wifi,
+    notConnected
+}
+
+public class InternetConnection {
+    public class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+          //  SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+
+        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags.connectionAutomatic
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+
+        return (isReachable && !needsConnection) ? true : false
+    }
+
+    public class func isConnectedToNetworkOfType() -> IJReachabilityType {
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+
+        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags.connectionAutomatic
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return .notConnected
+        }
+
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let isWWAN = (flags.rawValue & UInt32(SCNetworkReachabilityFlags.isWWAN.rawValue)) != 0
+
+        if isReachable && isWWAN {
+            return .wwan
+        }
+        if isReachable && !isWWAN {
+            return .wifi
+        }
+        return .notConnected
+    }
+}
