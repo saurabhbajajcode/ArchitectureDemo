4a11091c23283a584355eff64bd70b76a606c812
Saurabh Bajaj <saurabhbsd@gmail.com>
2021-02-13 18:08:50 +0530

    Initial Commit

---
+++ ArchitectureDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchitectureDemo
+//
+//  Created by Saurabh Bajaj on 13/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
